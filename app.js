0ea94c83cee2bd208fbd6865795432ee70793d25
Krish Patel <krispatel27x@gmail.com>
2025-04-02 23:20:45 +0530

    changed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Endpoint to download a file
 app.get("/rsa", (req, res) => {
