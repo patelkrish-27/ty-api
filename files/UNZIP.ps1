5a8790e268735795857055fd48f2c7f9da7f8d99
Krish Patel <krispatel27x@gmail.com>
2025-04-02 18:00:41 +0530

    BACK TO BAT

--- files/UNZIP.ps1
+++
@@ -1,5 +0,0 @@
-$zipFile = Read-Host "Enter ZIP file name (with extension)"
-$outputFolder = $PWD
-
-Expand-Archive -Path "$PWD\$zipFile" -DestinationPath $outputFolder -Force
-Write-Output "Unzip completed!"
